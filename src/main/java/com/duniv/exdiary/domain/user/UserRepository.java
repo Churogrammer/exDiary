a322f2c41e8f76bcc778980e531e11f9cfffc9de
churogrammer <m_sense1031@naver.com>
2023-04-13 22:44:19 +0900

    remove user

--- src/main/java/com/duniv/exdiary/domain/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.duniv.exdiary.domain.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, String> {
-    public UserEntity findByUserId(String userId);
-}
