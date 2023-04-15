355f8581ba44131b4603addcd4f4c32675650920
churogrammer <m_sense1031@naver.com>
2023-04-15 23:40:11 +0900

    테스트 수정

--- src/test/java/com/duniv/exdiary/service/EmployeeRegisterServiceTest.java
+++ src/test/java/com/duniv/exdiary/service/EmployeeRegisterServiceTest.java
@@ -1,13 +1,10 @@
 package com.duniv.exdiary.service;
 
-import com.duniv.exdiary.domain.employee.EmployeeRegisterDTO;
+import com.duniv.exdiary.domain.user.employee.EmployeeRegisterDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class EmployeeRegisterServiceTest {
