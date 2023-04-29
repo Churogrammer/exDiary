01ce614048761f0b40340d7afff9c8fc75730a49
churogrammer <m_sense1031@naver.com>
2023-04-30 00:14:39 +0900

    mod : redis 설정 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ extra["springCloudVersion"] = "2021.0.6"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    //implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-web")
     //implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 
